10653ef97bff8f593ddb1a95c049b83b65d44dde
BRutan <rutan.benjamin@gmail.com>
2021-04-08 01:27:34 -0400

    Finalized neural network implementation, tracking losses and backpropagation. Need to handle working with batches and proper backprop with batches.

--- mnist/NN/neural_network.py
+++ mnist/NN/neural_network.py
@@ -6,46 +6,120 @@
 # digits in MNIST dataset.
 
 from tqdm import trange
+from torch import flatten, float64, logit, zeros
+from torch.nn import Linear, Module, BCEWithLogitsLoss, ReLU, Sequential, Sigmoid, Softmax
 from torch.optim import SGD
-from torch.nn import Linear, Module, MSELoss, ReLU, Sequential, Sigmoid
 
-class NeuralNetwork(Module):
+class MNISTPredictor(Module):
     """
     * Neural network for training
     and testing MNIST dataset.
     """
-    def __init__(self, dims, loss = MSELoss):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, loss = BCEWithLogitsLoss()):
         """
         * Initialize model corresponding
         to input dimensions and that uses
         a particular learning rate.
+        Inputs:
+        * input_nodes: Number of input nodes (dimensions of each sampled tensor).
+        * hidden_nodes: tuple containing (hidden_layer_node_1, ..., hidden_layer_node_n).
+        * output_nodes: Number of output nodes (dimensions of output tensor).
+        * loss: Loss function.
         """
-        self.__model = Sequential([Linear(), ReLU, Linear(), ReLU])
+        super().__init__()
+        sequence = self.__GenerateSequence(input_nodes, hidden_nodes, output_nodes)
+        self.__model = Sequential(*sequence)
         self.__loss = loss
 
     ###############
+    # Properties:
+    ###############
+    @property
+    def InputSize(self):
+        return self.__inputsize
+    @property
+    def OutputSize(self):
+        return self.__outputsize
+    ###############
     # Interface Methods:
     ###############
-    def train(self, batches, epochs = 50, lr = .0001, verbose = False):
+    def encode_label(self, label):
+        """
+        * Transform numeric label to tensor for appropriate usage
+        in gradient descent.
+        """
+        if label.shape[0] > 1:
+            encoded = zeros([label.shape[0], 10], dtype=float64)
+            for num, lbl in enumerate(label):
+                encoded[num][lbl] = 1
+        else:
+            encoded = zeros([10], dtype=float64)
+            encoded[label[0]] = 1
+        return encoded
+
+    def convert_pass(self, fwd):
+        """
+        * Convert output probabilities into
+        prediction.
+        """
+        num = 0
+        max_prob = fwd.max().item()
+        for num in range(len(fwd)):
+            if fwd[num][0].item() == max_prob:
+                return num
+
+    def train(self, batches, epochs = 50, lr = .01, verbose = False, track = False):
         """
         * Train model using training batches.
         Inputs:
         * batches: Batcher with appropriately transformed
         datasets for neural network model.
         """
+        if track:
+            losses = []
         self.__optimizer = SGD(self.__model.parameters(), lr = lr)
         epoch_range = trange(0, epochs) if verbose else range(0, epochs)
+        batch_range = trange(0, len(batches)) if verbose else range(0, len(batches))
         for epoch in epoch_range:
-            for input, target in batches:
+            batch_iter = iter(batches)
+            for batch in batch_range:
+                input, target = next(batch_iter)
+                target = self.encode_label(target)
                 self.__optimizer.zero_grad()
                 output = self.__model(input)
-                loss = self.__loss(output, target)
+                loss = self.__loss(flatten(output), target)
                 loss.backward()
                 self.__optimizer.step()
+                if track:
+                    losses.append(loss.item())
+        if track:
+            return losses
 
     def forward(self, data):
         """
         * Pass data through the neural network
         to generate an (untransformed) prediction.
         """
-        pass
+        if data.shape[1] != self.__inputsize:
+            raise Exception('data must have .shape[1] == %s' % self.__inputsize)
+        return self.__model(data)
+
+    def __GenerateSequence(self, input_nodes, hidden_nodes, output_nodes):
+        """
+        * Generate layers for neural network.
+        """
+        self.__inputsize = input_nodes
+        self.__outputsize = output_nodes
+        layers = []
+        for num, hidden_dim in enumerate(hidden_nodes):
+            if num == 0:
+                layers.append(Linear(input_nodes, hidden_dim))
+            else:
+                layers.append(Linear(hidden_nodes[num - 1], hidden_dim))
+        layers.append(Linear(hidden_dim, output_nodes))
+        sequence = []
+        for layer in layers:
+            sequence.append(layer)
+            sequence.append(Sigmoid())
+        sequence.append(Softmax())
+        return sequence
--- mnist/main.py
+++ mnist/main.py
@@ -7,60 +7,74 @@
 # and labels, generate predictions
 # and display accuracy.
 
-from mnist import MNIST
-from NN.neural_network import NeuralNetwork
+import matplotlib.pyplot as plt
+from NN.neural_network import MNISTPredictor
 import numpy as np
 import os
+from sklearn.metrics import confusion_matrix
+from torch import flatten, FloatTensor
 from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-def load_data():
+normalize_data = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,)), flatten])
+#target_transform = transforms.Compose([])
+
+def load_data(batchsize):
     """
     * Load data from local mnist dataset (in Data folder).
     """
-    data = MNIST('Data')
-    # images, labels:
-    training = data.load_training()
-    testing = data.load_testing()
+    train = DataLoader(datasets.MNIST('data', train = True, download = True, transform = normalize_data), batch_size = batchsize)
+    test = DataLoader(datasets.MNIST('data', train = False, transform = normalize_data))
+    return train, test
 
-    return training, testing
-
-def transform_and_batch(training, testing):
-    """
-    * Transform the training and testing sets
-    for usage in neural network.
-    """
-    training = DataLoader(training, batch_size = 60, shuffle = True)
-    return training, testing
-
-def train_model(training, input_len):
+def train_model(train, input_nodes, hidden_nodes, num_outputs, epochs, lr, track):
     """
     * Train model using training set.
     """
-    model = NeuralNetwork(input_len)
-    model.train(training, verbose = True)
-    return model
+    model = MNISTPredictor(input_nodes, hidden_nodes, num_outputs)
+    losses = model.train(train, 1, lr, verbose = True, track = track)
+    return model, losses
 
-def test_model(model, testing, report_path):
+def plot_losses(losses, epochs, batchsize, lr):
+    """
+    * Plot losses by batch for visualization purposes.
+    """
+    plt.clf()
+    plt.title('MNIST Dataset Losses Epochs:%s BatchSize:%s LR:%s' % (epochs, batchsize, lr))
+    plt.scatter(list(range(len(losses))), losses)
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.savefig('Losses_%s_%s_%s.png' % (epochs, batchsize, str(lr).strip('.')))
+
+def test_model(model, test, report_path):
     """
     * Test model using testing set.
     """
-    predictions = []
-    for idx, input in enumerate(testing[0]):
-        label = testing[1][idx]
-        result = model.forward(input)
-        # Convert the forward pass into a result:
-        predictions.append(result)
-
-
+    labels = []
+    predicted = []
+    with open(report_path, 'w') as f:
+        f.write('Actual,Predicted')
+        for input, target in test:
+            pred = model.forward(input)
+            pred = model.convert_pass(pred)
+            # Convert the forward pass into a result:
+            labels.append(target)
+            predicted.append(predicted)
+            f.write('%s,%s' % (target, pred))
+    return confusion_matrix(predicted, labels)
 
 def main():
     """
     * Perform key steps in order.
     """
-    training, testing = load_data()
-    training, testing = transform_and_batch(training, testing)
-    model = train_model(training, len(testing[0][0]))
-    test_model(model, testing, "MNIST_Predictions.csv")
+    epochs = 1
+    batchsize = 60
+    lr = .01
+    train, test = load_data(batchsize)
+    model, losses = train_model(train, 784, (128, 64), 10, epochs, lr, True)
+    plot_losses(losses, epochs, batchsize, lr)
+    matr = test_model(model, test, "MNIST_Predictions.csv")
+    print(matr)
 
 if __name__ == '__main__':
     main()
